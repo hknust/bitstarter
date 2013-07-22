eeafeeb5246ed90cdaaa0c806598fe3761ad44d2
Holger Knust <holger.knust@certusview.com>
2013-07-22 00:24:40 -0400

    Homework 4 Part 2 Server

--- web.js
+++ web.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+console.log("Dir : " + __dirname + "/public");
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(request, response) {
   var b = fs.readFileSync('./index.html');
   response.send( b.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
